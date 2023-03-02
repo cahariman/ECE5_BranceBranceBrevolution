d8b744cfe28d8b35f3f003784f3f016c9db706ca
Christopher Andrew Hariman <cahariman@ucsb.edu>
2023-03-02 07:15:18 -0800

    updated LED_board_with_array.ino; TODOs include actually iterating through the LED board and using buttons

---
+++ LED_board_with_array.ino
@@ -0,0 +1,164 @@
+#include <FastLED.h>
+#define NUM_LEDS 60
+#define DATA_PIN 4
+
+CRGB leds[NUM_LEDS];
+
+// arrays that represent when stuff should happen; will come from C++ program
+// below example arrays mark that first button should be pressed 1000 ms after song starts
+// 2nd button should be pressed 1500ms after song starts
+// 3rd button should be pressed 2000ms after song starts
+// future: first element will have number of times; "size" of the array
+long long song1_key1_times[] = {-1, 1000, 4500, 7000};
+int w = 1;
+long long song1_key2_times[] = {-1, 1500, 6000, 7500};
+int a = 1;
+long long song1_key3_times[] = {-1, 2000, 5000, 8500};
+int s = 1;
+long long song1_key4_times[] = {-1, 3000, 8000, 9000};
+int d = 1;
+
+// these represent the next time a light should turn on; technically when a button should be pressed after the song starts
+unsigned long next_light_1 , next_light_2 , next_light_3, next_light_4 = 0;
+
+// function that will update time that light should turn on for next light using the next element of the arrays
+// these functions endup accessing out of array bounds, I think i can fix that by including the size of the array in the first element in the future
+void getNextTime_button1() {
+
+    // check next element of array and update the next time with the value
+    next_light_1 = song1_key1_times[w];
+    Serial.println("updated next_light1");
+    
+    // increment i so that next element is ready to be read when the function is called again
+    w++;
+}
+void getNextTime_button2() {
+    
+    // check next element of array and update the next time with the value
+    next_light_2 = song1_key2_times[a];
+    Serial.println("updated next_light2");
+    
+    // increment i so that next element is ready to be read when the function is called again
+    a++;
+}
+void getNextTime_button3() {
+    
+    // check next element of array and update the next time with the value
+    next_light_3 = song1_key3_times[s];
+    Serial.println("updated next_light3");
+    
+    // increment i so that next element is ready to be read when the function is called again
+    s++;
+}
+void getNextTime_button4() {
+    
+    // check next element of array and update the next time with the value
+    next_light_4 = song1_key4_times[d];
+    Serial.println("updated next_light4");
+    
+    // increment i so that next element is ready to be read when the function is called again
+    d++;
+}
+
+// function for debugging
+void Print_Next_Times() {
+    Serial.print("next_light_1: ");
+    Serial.println(next_light_1);
+    Serial.print("next_light_2: ");
+    Serial.println(next_light_2);
+    Serial.print("next_light_3: ");
+    Serial.println(next_light_3);
+    Serial.print("next_light_4: ");
+    Serial.println(next_light_4);
+}
+
+void setup() { 
+    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS); 
+
+    // create a variable for when the arduino starts running
+    long t_arduino_start= millis ();
+    getNextTime_button1();
+    getNextTime_button2();
+    getNextTime_button3();
+    getNextTime_button4();
+    Serial.begin(9600);
+
+    // debugging
+    Print_Next_Times();
+
+}
+
+// event based programming !
+void loop() {
+
+    // check if it is time to prompt for the first button
+    if ( millis() > next_light_1 ) {
+
+        // TODO
+        // iterate for column 1 with fastLED.h stuff; for loop
+        // at 9th or 10th row in for loop, do a button check and then score stuff
+
+        // debugging
+        Serial.println("NEXT");
+
+        // get next time from array
+        getNextTime_button1();
+
+        // debugging
+        Print_Next_Times();
+    }
+    if ( millis() > next_light_2 ) {
+
+        // TODO
+        // iterate for column 2 with fastLED.h stuff; for loop
+        // at 9th or 10th row in for loop, do a button check and then score stuff
+
+        // debugging
+        Serial.println("NEXT");
+
+        // get next time from array
+        getNextTime_button2();
+
+        // debugging
+        Print_Next_Times();
+    }
+    if ( millis() > next_light_3 ) {
+
+        // TODO
+        // iterate for column 3 with fastLED.h stuff; for loop
+        // at 9th or 10th row in for loop, do a button check and then score stuff
+
+        // debugging
+        Serial.println("NEXT");
+
+        // get next time from array
+        getNextTime_button3();
+
+        // debugging
+        Print_Next_Times();
+    }
+    if ( millis() > next_light_4 ) {
+
+        // TODO
+        // iterate for column 4 with fastLED.h stuff; for loop
+        // at 9th or 10th row in for loop, do a button check and then score stuff
+
+        // debugging
+        Serial.println("NEXT");
+
+        // get next time from array
+        getNextTime_button4();
+
+        // debugging
+        Print_Next_Times();
+    }
+
+    // for loop to iterate through 10 lights
+    // for(int i = 0; i < 10; i++) {
+    //     leds[i] = CRGB::MediumVioletRed; FastLED.show(); delay(200);
+    //     leds[i] = CRGB::Black; FastLED.show(); delay(200);    
+    // }
+	
+    // leds[1] = CRGB::MediumSeaGreen; wFastLED.show(); delay(300);
+    // leds[1] = CRGB::DarkOliveGreen; FastLED.show(); delay(300);
+}
