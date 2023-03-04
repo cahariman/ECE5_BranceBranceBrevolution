cbbe9531ce2fa6d181b9b4aca6acf971b4e6e95c
Christopher Andrew Hariman <cahariman@ucsb.edu>
2023-03-04 12:24:43 -0800

    added 4 seperate arrays

--- C++_for_preprocessing/songToArray.cpp
+++ C++_for_preprocessing/songToArray.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <chrono>
 #include <thread>
+#include "conio.h"
 
 using namespace std;
 using namespace std::chrono;
@@ -20,12 +21,14 @@ int main() {
 
     // TODO
     // play song
+    
     high_resolution_clock::time_point song_start_time = high_resolution_clock::now();
-    vector<int> time_of_press(1, -1);
-    vector<int> key_pressed(1,-1);
+    vector<long long> song1_key1_times(1, -1);
+    vector<long long> song1_key2_times(1,-1);
+    vector<long long> song1_key3_times(1, -1);
+    vector<long long> song1_key4_times(1,-1);
 
 
-    // TODO
     // while the song is playing, see when certain keys are pressed and record the time at which they are pressed into an array
     // this array will be printed into the output file
     // USE cstream and time functions 
@@ -36,14 +39,48 @@ int main() {
 
     char c;
     cout << "enter: ";
-    cin >> c;
+    // cin >> c;
+
+    high_resolution_clock::time_point time_now;
+    duration<long, milli> time_since_song_start;
+    while(c != 'b') {
+        if (kbhit()) {
+            c = getch();
+            cout << "You pressed: " << c << endl;
+            time_now = high_resolution_clock::now();
+            time_since_song_start = duration_cast<milliseconds>(time_now - song_start_time);
+            cout << time_since_song_start.count() << endl;
+            switch (c) {
+                case 'd':
+                    song1_key1_times.push_back(time_since_song_start.count());
+                    cout << "added to song1_key1_times" << endl;
+                    break;
+                case 'f':
+                    song1_key2_times.push_back(time_since_song_start.count());
+                    cout << "added to song1_key2_times" << endl;
+                    break;
+                case 'j':
+                    song1_key3_times.push_back(time_since_song_start.count());
+                    cout << "added to song1_key3_times" << endl;
+                    break;
+                case 'k':
+                    song1_key4_times.push_back(time_since_song_start.count());
+                    cout << "added to song1_key4_times" << endl;
+                    break;
+            }
+        }
+    }
     cout << "done" << endl;
-    high_resolution_clock::time_point time_now = high_resolution_clock::now();
-    duration<long, milli> time_since_song_start = duration_cast<milliseconds>(time_now - song_start_time);
-    cout << time_since_song_start.count() << endl;
-    time_of_press.push_back( time_since_song_start.count() );
-    key_pressed.push_back( c );
-    cout << "test1" << endl;
+
+
+    // high_resolution_clock::time_point time_now = high_resolution_clock::now();
+    // duration<long, milli> time_since_song_start = duration_cast<milliseconds>(time_now - song_start_time);
+    // cout << time_since_song_start.count() << endl;
+
+
+    // song1_key1_times.push_back( time_since_song_start.count() );
+    // song1_key2_times.push_back( c );
+    // cout << "test1" << endl;
     // while (time_since_song_start.count() < 50) {
     //     cout << "test2" << endl;
 
@@ -51,23 +88,38 @@ int main() {
     //     duration<double, milli> time_since_song_start = time_now - song_start_time;
     //     cout << time_since_song_start.count() << endl;
     //     // if (true) {
-    //     //     time_of_press.push_back( time_since_song_start.count() );
+    //     //     song1_key1_times.push_back( time_since_song_start.count() );
     //     // }
     // }
-    cout << "test3" << endl;
+    //cout << "test3" << endl;
+
+
     // Write to file
     outFS << endl;
-    outFS << "long long song1_times[] = {";
-    for (int i = 0; i < time_of_press.size() - 1; i++) {
-        outFS << time_of_press.at(i) << ", ";
+    outFS << "long long song1_key1_times[] = {";
+    for (int i = 0; i < song1_key1_times.size() - 1; i++) {
+        outFS << song1_key1_times.at(i) << ", ";
     }
-    outFS << time_of_press.at(time_of_press.size() - 1) << "};" << endl;
+    outFS << song1_key1_times.at(song1_key1_times.size() - 1) << "};" << endl;
 
-    outFS << "long long song1_keys[] = {";
-    for (int i = 0; i < key_pressed.size() - 1; i++) {
-        outFS << key_pressed.at(i) << ", ";
+    outFS << "long long song1_key2_times[] = {";
+    for (int i = 0; i < song1_key2_times.size() - 1; i++) {
+        outFS << song1_key2_times.at(i) << ", ";
     }
-    outFS << key_pressed.at(key_pressed.size() - 1) << "};" << endl;
+    outFS << song1_key2_times.at(song1_key2_times.size() - 1) << "};" << endl;
+
+    outFS << "long long song3_key1_times[] = {";
+    for (int i = 0; i < song1_key3_times.size() - 1; i++) {
+        outFS << song1_key3_times.at(i) << ", ";
+    }
+    outFS << song1_key3_times.at(song1_key3_times.size() - 1) << "};" << endl;
+
+    outFS << "long long song1_key4_times[] = {";
+    for (int i = 0; i < song1_key4_times.size() - 1; i++) {
+        outFS << song1_key4_times.at(i) << ", ";
+    }
+    outFS << song1_key4_times.at(song1_key4_times.size() - 1) << "};" << endl;
+
     outFS << "Hello" << endl;
     outFS << "1 2 3" << endl;
 
